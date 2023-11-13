9e9feaee9e485d82eaf738802d24dac1b6b8d426
Amr <AmrSaad1>
2023-11-13 20:19:40 +0200

    ChangeColorback

--- lib/Home.dart
+++ lib/Home.dart
@@ -178,8 +178,8 @@ setState(() {
                           min: 120.0,
                           max: 200.0,
                           activeColor: Color(0xffEB1555),
-                    //    inactiveColor: Color(0xff8D8E98),
-                          inactiveColor: Colors.black,
+                       inactiveColor: Color(0xff8D8E98),
+                        //  inactiveColor: Colors.black,
                           onChanged: (newValue){
                         setState(() {
                           Height = newValue.round() ;
